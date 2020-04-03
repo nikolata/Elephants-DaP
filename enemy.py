9becc498f42986c9e39e4766ceb3f6bc94680860
nikolata <npmetodiev@gmail.com>
2020-04-03 19:23:38 +0300

    pep-8 changes

--- enemy.py
+++ enemy.py
@@ -1,20 +1,20 @@
 from entity import Entity
 
+
 class Enemy(Entity):
-    def __init__(self,health,mana,damage):
-        if health<=0 or mana<0 or damage<0:
+    def __init__(self, health, mana, damage):
+        if health <= 0 or mana < 0 or damage < 0:
             raise ValueError
-        super().__init__(health,mana)
+        super().__init__(health, mana)
         self.damage = damage
-    def take_mana(self,mana_potion):
-        if mana_potion <=0:
+
+    def take_mana(self, mana_potion):
+        if mana_potion <= 0:
             raise ValueError
-        self._current_mana +=mana_potion
-    def attack(self, by = None):
+        self._current_mana += mana_potion
+
+    def attack(self, by=None):
         if by is None:
             return self.damage
         else:
-            return super().attack(by = by)
-
-
-enemy = Enemy(100,40,20)
+            return super().attack(by=by)
